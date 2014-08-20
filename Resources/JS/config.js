83ca769b43caecdcd564a1e20dc527e5a210535d
W. Ryan Nestor <nestor@fuzzy-math.com>
2014-08-20 10:19:28 -0400

    worked on config loader

--- Resources/JS/config.js
+++ Resources/JS/config.js
@@ -27,23 +27,26 @@ try{
 	settings = require(homeDir+config);
 	console.log("Using config.json Settings: ", settings);
 	
-	role = settings.role;
-	opponent = settings.opponent;
-	position = settings.position;
-	ai = settings.ai;
-	url = settings.url;
-	curScreen = settings.curScreen;
-	name = "null";
-	curPhase = 0;
+
 }
 catch(e){
 	settings = require("./config.json");
-	console.log("Using DEFAULT cfg: ", e);
-	role = "seller";
+	console.log("Using DEFAULT cfg: ", settings);
+	/*role = "seller";
 	opponent = "buyer";
 	position = 0;
 	ai = false;
-	curPhase = 0;
+	curPhase = 0;*/
 }
+
+
+role = settings.role;
+opponent = settings.opponent;
+position = settings.position;
+ai = settings.ai;
+url = settings.url;
+curScreen = settings.curScreen;
+name = "null";
+curPhase = 0;
 
 // --------------------------- //
